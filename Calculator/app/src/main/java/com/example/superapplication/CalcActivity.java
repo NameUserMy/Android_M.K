4c948e073db556253868fc38a4a99bc2ac5ac9c6
SecondDeveloper <SecondDeveloper@example.com>
2025-04-07 16:22:54 +0300

    Simple Arithmetics

--- Calculator/app/src/main/java/com/example/superapplication/CalcActivity.java
+++ Calculator/app/src/main/java/com/example/superapplication/CalcActivity.java
@@ -1,6 +1,7 @@
 package com.example.superapplication;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,6 +19,8 @@ public class CalcActivity extends AppCompatActivity {
     private TextView tvExpression;
     private TextView tvResult;
     private String zero;
+    private boolean isSecond;
+    private boolean isEqual;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +32,13 @@ public class CalcActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        tvExpression=findViewById(R.id.calc_tv_expression);
-        tvResult=findViewById(R.id.calc_tv_result);
-        zero= getString(R.string.calc_btn_0);
-        Button btnC=findViewById(R.id.calc_btn_c);
+        tvExpression = findViewById(R.id.calc_tv_expression);
+        tvResult = findViewById(R.id.calc_tv_result);
+        zero = getString(R.string.calc_btn_0);
+
+        Button btnC = findViewById(R.id.calc_btn_c);
         btnC.setOnClickListener(this::onClearClick);
+
         findViewById(R.id.calc_btn_0).setOnClickListener(this::onDigitClick);
         findViewById(R.id.calc_btn_1).setOnClickListener(this::onDigitClick);
         findViewById(R.id.calc_btn_2).setOnClickListener(this::onDigitClick);
@@ -44,43 +49,152 @@ public class CalcActivity extends AppCompatActivity {
         findViewById(R.id.calc_btn_7).setOnClickListener(this::onDigitClick);
         findViewById(R.id.calc_btn_8).setOnClickListener(this::onDigitClick);
         findViewById(R.id.calc_btn_9).setOnClickListener(this::onDigitClick);
+        findViewById(R.id.calc_btn_dot).setOnClickListener(this::onDigitClick);
+
+
+        findViewById(R.id.calc_btn_add).setOnClickListener(this::onArithmeticsClick);
+        findViewById(R.id.calc_btn_mul).setOnClickListener(this::onArithmeticsClick);
+        findViewById(R.id.calc_btn_div).setOnClickListener(this::onArithmeticsClick);
+        findViewById(R.id.calc_btn_sub).setOnClickListener(this::onArithmeticsClick);
+        findViewById(R.id.calc_btn_eq).setOnClickListener(this::onArithmeticsClick);
+        findViewById(R.id.calc_btn_backspace).setOnClickListener(this::onBackspaceClick);
 
         onClearClick(btnC);
     }
+
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
-
         super.onSaveInstanceState(outState);
-        outState.putCharSequence("result",tvResult.getText());
-
-
+        outState.putCharSequence("result", tvResult.getText());
+        outState.putCharSequence("expretions", tvExpression.getText());
     }
+
     @Override
-    protected
-    void onRestoreInstanceState(@NonNull Bundle savedInstanceState)
-    {
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         tvResult.setText(savedInstanceState.getCharSequence("result"));
+        tvExpression.setText(savedInstanceState.getCharSequence("expretions"));
 
     }
 
-    private void onDigitClick(View view){
+    private void onArithmeticsClick(View view) {
+        String operation = view.getTag().toString();
 
-        String result=tvResult.getText().toString();
-        if(result.equals(zero)){
+        if (tvExpression.getText().toString().isBlank()) {
+            tvExpression.setText(tvResult.getText().toString() + " " + operation);
 
-            result="";
+        } else {
+
+            arithmeticsOperation(operation);
+
         }
-        result+=((Button)view).getText();
-        tvResult.setText(result);
+    }
+    private void onDigitClick(View view) {
 
+
+        if (isEqual) {
+
+            tvExpression.setText("");
+            isEqual = false;
+            tvResult.setText("");
+
+        }
+
+        String result = tvResult.getText().toString();
+
+        if (!tvExpression.getText().toString().isBlank()) {
+
+
+            if (!this.isSecond) {
+                result = "";
+                isSecond = true;
+            }
+            result += ((Button) view).getText();
+            tvResult.setText(result);
+
+        }else{
+
+            if (!((Button) view).getText().toString().equals(".")&&result.equals(zero)) {
+
+                result = "";
+            }
+            result += ((Button) view).getText();
+            tvResult.setText(result);
+
+        }
 
     }
-    private void onClearClick(View view){
+
+    private void onClearClick(View view) {
 
 
         tvExpression.setText("");
         tvResult.setText(zero);
+        this.isSecond = false;
+
+    }
+    private void onBackspaceClick(View view){
+
+      String backspaceText= tvResult.getText().toString();
+
+      tvResult.setText(backspaceText.substring(0,backspaceText.length()-1));
+
+
+    }
+    private void arithmeticsOperation(String operation) {
+
+        String[] expression = tvExpression.getText().toString().split(" ");
+        String result = String.valueOf(result(expression, tvResult.getText().toString()));
+        if (operation.equals("=")) {
+
+            tvExpression.setText(tvExpression.getText().toString() + " " + tvResult.getText().toString() + "" + operation);
+            tvResult.setText(result);
+            isEqual = true;
+
+
+        } else {
+            tvExpression.setText(result + " " + operation);
+            tvResult.setText(result);
+
+        }
+        isSecond = false;
+
+    }
+
+    private String result(String[] parseExpretion, String second) {
+
+        double firstOperant = Double.parseDouble(parseExpretion[0]);
+        double secondOperant = Double.parseDouble(second);
+
+        String value = "";
+        switch (parseExpretion[1]) {
+
+            case "+":
+                value = String.valueOf(firstOperant + secondOperant);
+                break;
+            case "-":
+                value = String.valueOf(firstOperant - secondOperant);
+                break;
+            case "x":
+                value = String.valueOf(firstOperant * secondOperant);
+                break;
+            case "/":
+                value = String.valueOf(firstOperant / secondOperant);
+                break;
+        }
+
+        String[] isDouble = value.split("\\.");
+
+        if (Double.parseDouble(isDouble[1]) == 0) {
+
+            return isDouble[0];
+        } else {
+            if(value.length()>12){
+
+                return   value.substring(0,11);
+            }
+            return value;
+        }
 
     }
 }
